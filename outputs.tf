d180e8b5c9e3a4efad1b9b1af67b6fdd1c1e9b0b
gitrabii <rabii.abed@hotmail.fr>
2025-04-03 15:29:31 +0100

    test

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+
+output "webapp_url" { 
+  description = "URL de l'Azure Web App déployée" 
+  value       = module.webapp.webapp_url 
+} 
---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" { 
+  description = "Nom du Resource Group" 
+  default     = "my-webapp-rg" 
+} 
+ 
+variable "location" { 
+  description = "Région Azure de déploiement" 
+  default     = "France Central" 
+} 
+ 
+variable "app_service_plan_name" { 
+  description = "Nom du service plan pour l'App Service" 
+  default     = "my-free-appservice-plan" 
+} 
+ 
+variable "webapp_name" { 
+  description = "Nom de l'Azure Web App" 
+  default     = "my-free-webapp" 
+}
