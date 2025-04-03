247b64da2aae1d98e9311f4deccefc655f9e2a8f
gitrabii <rabii.abed@hotmail.fr>
2025-04-03 15:53:07 +0100

    tst

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform { 
-  backend "azurerm" { 
-    resource_group_name  = "dev-rg" 
-    storage_account_name = "tfdevbackendrabii" 
-    container_name       = "tfstate" 
-    key                  = "terraform.tfstate" 
-  } 
-} 
+# terraform { 
+#   backend "azurerm" { 
+#     resource_group_name  = "dev-rg" 
+#     storage_account_name = "tfdevbackendrabii" 
+#     container_name       = "tfstate" 
+#     key                  = "terraform.tfstate" 
+#   } 
+# } 
